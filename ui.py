d70e555366894a9879520a0fb5774e24dc678241
acamilo <alex.camilo@gmail.com>
2022-02-22 16:50:29 -0500

    finished wiring up UI state

--- ui.py
+++ ui.py
@@ -30,24 +30,68 @@ class DynTestApp:
 
         self.assign_right_button = self.builder.get_object("motor-assign-right-button")
         self.assign_right_button.connect("clicked", self.assign_right_button_clicked)
+
+        # Test Motor selection buttons
+        self.test_left_button = self.builder.get_object("motor-test-left-button")
+        self.test_left_button.connect("clicked", self.test_left_button_clicked)
+
+        self.test_right_button = self.builder.get_object("motor-test-right-button")
+        self.test_right_button.connect("clicked", self.test_right_button_clicked)
+
+        # Return to menu buttons
+        self.assign_ok_menu = self.builder.get_object("assign-ok-menu")
+        self.assign_ok_menu.connect("clicked", self.return_to_main_menu)
+        self.assign_fail_menu = self.builder.get_object("assign-fail-menu")
+        self.assign_fail_menu.connect("clicked", self.return_to_main_menu)
+        self.testing_motor_menu = self.builder.get_object("testing-motor-menu")
+        self.testing_motor_menu.connect("clicked", self.stop_motor_test)
+
         self.window.set_default_size(240, 320)
         self.window.show()
-        self.window.fullscreen()
+        #self.window.fullscreen()
 
+    def return_to_main_menu(self,widget):
+        self.stack.set_visible_child_name("menu-assign-test")
 
     def assign_motor_clicked(self,widget):
         print("assign Click")
-        self.stack.set_visible_child_name("motor-assign-right-left-menu")
+        self.stack.set_visible_child_name("motor-assign-menu")
 
     def test_motor_clicked(self,widget):
         print("test Click")
+        self.stack.set_visible_child_name("motor-test-menu")
 
     def assign_left_button_clicked(self,widget):
         print("Assign Left")
+        self.assign_motor(1)
 
     def assign_right_button_clicked(self,widget):
         print("Assign Right")
+        self.assign_motor(2)
+        
 
+    def test_left_button_clicked(self,widget):
+        print("Test Left")
+        self.test_motor(1)
+
+    def test_right_button_clicked(self,widget):
+        print("Test Right")
+        self.test_motor(2)
+
+
+    def assign_motor(self,id):
+        print("Assigning ID: %s to motor"%(str(id)))
+        if True:
+           self.stack.set_visible_child_name("assign-ok-menu") 
+        else:
+           self.stack.set_visible_child_name("assign-fail-menu")
+
+    def test_motor(self,id):
+        print("Testing Motor ID: %s"%(str(id)))
+        self.stack.set_visible_child_name("testing-motor-menu")
+    
+    def stop_motor_test(self,widget):
+        self.stack.set_visible_child_name("menu-assign-test")
 
     def on_destroy(self, widget):
         print("Destroyed!")
